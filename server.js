336ba3abbba63943340cb7e75ee46b2310e6fb19
James-Suresh <jamesblesson@gmail.com>
2023-04-10 13:54:54 -0400

    MongoDB set up

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require("mongoose")
 const app = express()
-const port = 3000
+const port = 5000
 app.use(cors)
+require('dotenv').config();
+
+const uri = process.env.MDB_URI
+console.log(uri)
+async function connect(){
+    try {
+        await mongoose.connect(uri)
+        console.log("Connected to mongodb")
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+connect()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+mongoose.connection.once('open',()=>{
+    console.log("Connected to mongodb")
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+      })
 })
