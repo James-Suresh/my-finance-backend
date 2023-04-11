0bc7fc8e335a01aaccfbb459ebe95af3674eb10d
James-Suresh <jamesblesson@gmail.com>
2023-04-11 02:41:39 -0400

    Testing deployment

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const cors = require('cors')
-const mongoose = require("mongoose")
+//const cors = require('cors')
+//const mongoose = require("mongoose")
 const app = express()
 
 const bcrypt = require('bcrypt')
-app.use(cors())
+//app.use(cors())
 app.use(express.json());
-const User = require('./models/User');
+//const User = require('./models/User');
 require('dotenv').config();
 const port = process.env.PORT || 80
 //const uri = process.env.MDB_URI
