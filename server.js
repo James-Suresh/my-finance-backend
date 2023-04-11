88ac970fbf117c9affb9fb8a5bd3177fc3078630
James-Suresh <jamesblesson@gmail.com>
2023-04-11 02:50:01 -0400

    Testing cors

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
-//const cors = require('cors')
+const cors = require('cors')
 const mongoose = require("mongoose")
 const app = express()
 
 const bcrypt = require('bcrypt')
-//app.use(cors())
+app.use(cors())
 app.use(express.json());
 //const User = require('./models/User');
 require('dotenv').config();
